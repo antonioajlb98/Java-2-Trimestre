ed95cc4003057ada5257f87e233246c28d002707
unknown <antonioajlb98@gmail.com>
2022-01-25 12:58:06 +0100

    Interfaces

---
+++ NewProyect Almacen/src/model/IDrink.java
@@ -0,0 +1,10 @@
+package model;
+
+public interface IDrink {
+	String getName();
+	Float getPrice();
+	void setName(String name);
+	void setPrice(Float price);
+	boolean equals(Object o);
+	String toString();
+}
